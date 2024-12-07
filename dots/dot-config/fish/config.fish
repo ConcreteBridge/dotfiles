89eae2a1a774fdeabc83d5ed7a489d2714405c97
CSJP <conradspark@gmail.com>
2024-12-06 21:43:37 -0600

    Add mise remove atuin
    Atuin was slowing me down :(

--- dots/dot-config/fish/config.fish
+++ dots/dot-config/fish/config.fish
@@ -1,6 +1,7 @@
 if status is-interactive
     set fish_greeting
 
-    atuin init fish | source
-    zoxide init fish | source
+    # atuin init fish    | source
+    mise activate fish | source
+    zoxide init fish   | source
 end
